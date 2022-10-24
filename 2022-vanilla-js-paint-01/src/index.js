002e6d7ab1854460609793d7e502b169ea7d1fb3
topcircle <topcircle@gmail.com>
2022-10-25 03:56:54 +0900

    vanillaJS-Paint #01

---
+++ 2022-vanilla-js-paint-01/src/index.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+// ctx.fillRect(50, 50, 100, 200);
+
+ctx.arc(300, 150, 100, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(260, 120, 15, Math.PI, 2 * Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(340, 120, 15, Math.PI, 2 * Math.PI);
+ctx.fillStyle = "green";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(300, 160, 50, 0, Math.PI);
+ctx.fillStyle = "red";
+ctx.fill();
+
+ctx.beginPath();
+ac;
+ctx.fillStyle = "black";
+ctx.fillRect(250, 270, 100, 300);
+
+ctx.beginPath();
+ctx.fillRect(205, 270, 25, 150);
+ctx.fillRect(370, 270, 25, 150);
